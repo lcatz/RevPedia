1546380ced3dafbe7a86f1094851d192b378a511
Robert Thompson <farellth@gmail.com>
2021-03-11 16:00:18 -0500

    added mock data and selectors

--- components/BatchListComponent.tsx
+++ components/BatchListComponent.tsx
@@ -1,12 +1,75 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { Card } from 'react-native-elements';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function BatchListComponent() {
+    const keyExtractor = (item: object, index: number) => {
+        return index.toString();
+    };
+    const batches = [
+        {
+            name: 'Batch 1',
+        },
+        {
+            name: 'Batch 2'
+        },
+        {
+            name: 'Batch 3'
+        },
+        {
+            name: 'Batch 4'
+        },
+        {
+            name: 'Batch 5'
+        },
+        {
+            name: 'Batch 6'
+        },
+        {
+            name: 'Batch 7'
+        },
+        {
+            name: 'Batch 8'
+        },
+        {
+            name: 'Batch 9'
+        },
+        {
+            name: 'Batch 10'
+        },
+        {
+            name: 'Batch 11'
+        },
+        {
+            name: 'Batch 12'
+        }
+    ]
+
+    const batchCard = (params: any) => {
+        return (
+            <Pressable onPress={() => handleBatchSelect(params.item)}>
+                <Card>
+                    <Text>{params.item.name}</Text>
+                </Card>
+            </Pressable>
+        );
+    };
+
+    function handleBatchSelect(batch: any) {
+        console.log(batch);
+    }
+
     return (
         <View>
             <Text>
                 Batch List Screen
             </Text>
+            <FlatList
+                data={batches}
+                renderItem={batchCard}
+                keyExtractor={keyExtractor}
+            />
         </View>
     )
 }
